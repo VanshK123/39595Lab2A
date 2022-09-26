82ff51be3499ff7ca971a456a5d787651cf54db0
Vansh Khanna <khanna57@ecegrid-thin1.ecn.purdue.edu>
2022-09-26 14:52:39 -0400

    added hash map cpp

---
+++ hash_map.cpp
@@ -0,0 +1,48 @@
+#include <optional>
+#include <stddef.h>
+#include <stdlib.h>
+#include "hash_map.h"
+
+hash_map::hash_map(size_t capacity){
+
+}
+
+hash_map::hash_map(const hash_map &other){
+
+}
+
+hash_map::hash_map &operator=(const hash_map &other){
+
+}
+
+void hash_map::insert(int key, float value){
+
+}
+
+std::optional<float> hash_map::get_value(int key) const{
+
+}
+
+bool hash_map::remove(int key){
+    
+}
+
+size_t hash_map::get_size() const{
+
+}
+
+ size_t hash_map::get_capacity() const{
+
+ }
+
+ void hash_map::get_all_keys(int* keys){
+
+ }
+
+void hash_map::get_bucket_sizes(size_t * buckets){
+
+}
+
+hash_map::~hash_map(){
+    
+}
